653987819e2ae67197d00bb381196194ff2348ea
Nilvaa <avlinantony26@gmail.com>
2026-08-18 15:56:21 +0530

    test crud worked

---
+++ backend/app/main.py
@@ -0,0 +1,82 @@
+from fastapi import FastAPI,Depends,HTTPException
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+
+from app.database import get_db
+from app.models.document import Document
+from app.schemas.document import DocumentCreate
+
+app=FastAPI(
+    title="Agentic RAG API",
+    version="1.0.0"
+)
+
+from app.database import engine,Base
+Base.metadata.create_all(bind=engine)
+@app.get("/")
+def root():
+    return {"message":"Agentic RAG API is running"}
+
+@app.get("/health")
+def health_check(db:Session=Depends(get_db)):
+    db.execute(text("SELECT 1"))
+    return {
+        "status":"healthy",
+        "database":"connected"
+    }
+
+@app.post("/documents")
+def create_document(
+    document:DocumentCreate,
+    db: Session = Depends(get_db)
+):
+    new_document=Document(
+        filename=document.filename
+    )
+    db.add(new_document)
+    db.commit()
+    db.refresh(new_document)
+
+    return new_document
+
+@app.get("/documents")
+def get_documents(db:Session=Depends(get_db)):
+    documents=db.query(Document).all()
+    return documents
+
+@app.get("/documents/{document_id}")
+def get_document(
+    document_id:int,
+    db: Session=Depends(get_db)
+):
+    document=db.query(Document).filter(
+        Document.id==document_id
+    ).first()
+
+    if document is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Document not found"
+        )
+    return document
+
+@app.delete("/documents/{document_id}")
+def delete_document(
+    document_id:int,
+    db: Session=Depends(get_db)
+):
+    document=db.query(Document).filter(
+        Document.id==document_id
+    ).first()
+
+    if document is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Document not found"
+        )
+    db.delete(document)
+    db.commit()
+
+    return {
+        "message":"Document deleted successfully"
+    }
---
+++ backend/app/models/document.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from sqlalchemy import DateTime,String
+from sqlalchemy.orm import Mapped,mapped_column
+
+from app.database import Base
+
+class Document(Base):
+    __tablename__= "documents"
+
+    id: Mapped[int]=mapped_column(
+        primary_key=True,
+        index=True
+    )
+
+    filename: Mapped[str]=mapped_column(
+        String(255),
+        nullable=False
+    )
+
+    created_at: Mapped[datetime]=mapped_column(
+        DateTime,
+        default=datetime.utcnow
+    )
